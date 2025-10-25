815e11c246f468a02e28fabc15c1ae7535fd6496
Muhammad Uzair Arif <uzairarif28@gmail.com>
2025-10-25 20:01:36 +0500

    Routing

---
+++ Routing/Convention/ConventionBasedRouting/ConventionBasedRouting/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConventionBasedRouting.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public int RouteWithId(int id)
+        {
+            //later we pass this id to view
+
+            return id;
+        }
+    }
+}
